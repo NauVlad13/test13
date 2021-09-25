0c7c9627e81bcc6538e860d621cbd75fcea93023
NauVlad13 <87044132+NauVlad13@users.noreply.github.com>
2021-09-25 10:21:19 +0300

    Update Copy.sh

--- Copy.sh
+++ Copy.sh
@@ -4,9 +4,9 @@ cp nginx.conf /etc/nginx/nginx.conf
 cp nginx.conf.default /etc/nginx/nginx.conf.default
 cp default.conf /etc/nginx/conf.d/default.conf
 cp upstream.conf /etc/nginx/conf.d/upstream.conf
-cp test8081 /var/www/test8081
-cp test8082 /var/www/test8082
-cp test8083 /var/www/test8083
+cp -R test8081 /var/www/test8081
+cp -R test8082 /var/www/test8082
+cp -R test8083 /var/www/test8083
 cp httpd.conf /etc/httpd/conf/httpd.conf
 cp prometheus.service /etc/systemd/system/prometheus.service
 cp alertmanager.service /etc/systemd/system/alertmanager.service
