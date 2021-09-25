0e21043b1270c6e23609d3b5c999117a3ab6c1b2
NauVlad13 <87044132+NauVlad13@users.noreply.github.com>
2021-09-25 06:05:19 +0300

    Create Copy.sh

---
+++ Copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp nginx.conf /etc/nginx/nginx.conf
+cp nginx.conf.default /etc/nginx/nginx.conf.default
+cp default.conf /etc/nginx/conf.d/default.conf
+cp upstream.conf /etc/nginx/conf.d/upstream.conf
+cp test8081 /var/www/test8081
+cp test8082 /var/www/test8082
+cp test8083 /var/www/test8083
+cp httpd.conf /etc/httpd/conf/httpd.conf
+cp prometheus.service /etc/systemd/system/prometheus.service
+cp alertmanager.service /etc/systemd/system/alertmanager.service
+cp node_exporter.service /etc/systemd/system/node_exporter.service
